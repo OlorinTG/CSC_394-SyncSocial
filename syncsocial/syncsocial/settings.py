3daa0a93e42f67e7549e8992d68e95fea66e57ed
Ubuntu <ubuntu@ip-172-31-40-168.us-east-2.compute.internal>
2023-05-11 18:21:19 +0000

    Add new feature

--- syncsocial/syncsocial/settings.py
+++ syncsocial/syncsocial/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_ac7yvbm3mk2p%jegozx_^o7b#owndqa)-(_f--1kli=yaz=*j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
